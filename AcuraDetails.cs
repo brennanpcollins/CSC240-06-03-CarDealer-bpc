5804974ebf157db4480f83d8b6f24c3e53dc5261
Brennan Collins <BCOLLI7351@my.sullivan.edu>
2021-11-16 21:23:04 -0500

    Changed the Name, but this is the improved GUI.

---
+++ AcuraDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC240_06_03_CarDealer_bpc
+{
+    public partial class AcuraDetails : Form
+    {
+        public AcuraDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- AcuraNSX.Designer.cs
+++ AcuraNSX.Designer.cs
@@ -34,6 +34,7 @@ namespace CSC240_06_03_CarDealer_bpc
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.UxAcuraImage = new System.Windows.Forms.PictureBox();
             this.UxNSXDetails = new System.Windows.Forms.Label();
+            this.UxAcuraDetailButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.UxAcuraImage)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,26 +54,39 @@ namespace CSC240_06_03_CarDealer_bpc
             // 
             // UxAcuraImage
             // 
+            this.UxAcuraImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.UxAcuraImage.Image = ((System.Drawing.Image)(resources.GetObject("UxAcuraImage.Image")));
-            this.UxAcuraImage.Location = new System.Drawing.Point(31, 378);
+            this.UxAcuraImage.Location = new System.Drawing.Point(-6, 681);
             this.UxAcuraImage.Name = "UxAcuraImage";
-            this.UxAcuraImage.Size = new System.Drawing.Size(603, 343);
+            this.UxAcuraImage.Size = new System.Drawing.Size(673, 377);
             this.UxAcuraImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.UxAcuraImage.TabIndex = 1;
             this.UxAcuraImage.TabStop = false;
+            this.UxAcuraImage.Click += new System.EventHandler(this.UxAcuraImage_Click);
             // 
             // UxNSXDetails
             // 
             this.UxNSXDetails.AutoSize = true;
             this.UxNSXDetails.BackColor = System.Drawing.Color.Transparent;
-            this.UxNSXDetails.Font = new System.Drawing.Font("Forte", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UxNSXDetails.Font = new System.Drawing.Font("Forte", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.UxNSXDetails.ForeColor = System.Drawing.Color.Black;
-            this.UxNSXDetails.Location = new System.Drawing.Point(640, 265);
+            this.UxNSXDetails.Location = new System.Drawing.Point(765, 254);
             this.UxNSXDetails.Name = "UxNSXDetails";
-            this.UxNSXDetails.Size = new System.Drawing.Size(572, 416);
+            this.UxNSXDetails.Size = new System.Drawing.Size(656, 82);
             this.UxNSXDetails.TabIndex = 2;
-            this.UxNSXDetails.Text = "Price: $157,500 USD\r\n\r\n\r\n\r\nThis new car is the remake \r\nof the classic NSX model." +
-    " \r\nGet this new beauty while \r\nyou can! ";
+            this.UxNSXDetails.Text = "Price: $157,500 USD\r\n";
+            // 
+            // UxAcuraDetailButton
+            // 
+            this.UxAcuraDetailButton.BackColor = System.Drawing.Color.Transparent;
+            this.UxAcuraDetailButton.Font = new System.Drawing.Font("Forte", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UxAcuraDetailButton.Location = new System.Drawing.Point(-6, 681);
+            this.UxAcuraDetailButton.Name = "UxAcuraDetailButton";
+            this.UxAcuraDetailButton.Size = new System.Drawing.Size(673, 98);
+            this.UxAcuraDetailButton.TabIndex = 3;
+            this.UxAcuraDetailButton.Text = "Click here for info.";
+            this.UxAcuraDetailButton.UseVisualStyleBackColor = false;
+            this.UxAcuraDetailButton.Click += new System.EventHandler(this.UxAcuraDetailButton_Click);
             // 
             // AcuraNSX
             // 
@@ -80,12 +94,13 @@ namespace CSC240_06_03_CarDealer_bpc
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1198, 794);
+            this.ClientSize = new System.Drawing.Size(1420, 1057);
+            this.Controls.Add(this.UxAcuraDetailButton);
             this.Controls.Add(this.UxNSXDetails);
             this.Controls.Add(this.UxAcuraImage);
             this.Controls.Add(this.UxNSXDescriptionLabel);
             this.Name = "AcuraNSX";
-            this.Text = "AcuraNSX";
+            this.Text = "2021 Acura NSX";
             ((System.ComponentModel.ISupportInitialize)(this.UxAcuraImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -98,5 +113,6 @@ namespace CSC240_06_03_CarDealer_bpc
         private System.Windows.Forms.PrintDialog printDialog1;
         private System.Windows.Forms.PictureBox UxAcuraImage;
         private System.Windows.Forms.Label UxNSXDetails;
+        private System.Windows.Forms.Button UxAcuraDetailButton;
     }
 }
